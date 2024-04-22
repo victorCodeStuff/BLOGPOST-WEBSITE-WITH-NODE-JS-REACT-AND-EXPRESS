aa53d41fb9739fea48456d364eb719e904d574d7
victorCodeStuff <victorcodestuff@outlook.com>
2024-04-22 23:05:36 +0000

    User authentication done

--- api/dbServer.cjs
+++ api/dbServer.cjs
@@ -64,6 +64,37 @@ app.post("/createUser", async (req, res) => {
    });
  });
 });
+app.post("/login", (req,res)=>{
+  const user =req.body.name
+  const password = req.body.password
+
+  db.getConnection(async (err, connection)=>{
+    if (err) throw (err)
+    const sqlSearch = "Select * from userTable where user = ?"
+    const search_query = mysql.format(sqlSearch,[user])
+
+    await connection.query(search_query, async (err,result)=>{
+      connection.release()
+      if(err) throw (err)
+      
+      if (result.length == 0){
+        console.log("-------> User does not exist")
+        res.sendStatus(404)
+      }else {
+        const hashedPassword = result[0].password
+
+        if (await bcrypt.compare(password, hashedPassword)){
+          console.log("-------> Login Sucesfully")
+          res.send(`${user} is logged in!`)
+        }
+        else {
+          console.log("-------> Password Incorrect")
+          res.send ("Password Incorrect")
+        }
+      }
+    })
+  })
+})
 
 const port = process.env.PORT;
 
